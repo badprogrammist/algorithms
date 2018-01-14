6f2550507cad61a6ae3d785b90c3395fbad47149
ildar <badprogrammist@gmail.com>
2018-01-14 21:40:03 +0500

    Solve 5.9

---
+++ src/main/java/ru/ildar/algorithm/graph/tasks/EvaluatingExpression.java
@@ -0,0 +1,136 @@
+package ru.ildar.algorithm.graph.tasks;
+
+import ru.ildar.algorithm.graph.DepthFirstRecursiveTraversal;
+import ru.ildar.algorithm.graph.Graph;
+import ru.ildar.algorithm.graph.GraphTraversal;
+
+import java.util.function.BiFunction;
+
+/**
+ * @author Ildar Gafarov (ildar.gafarov.ufa@gmail.com)
+ *         <p>
+ *         5-9. [3] Suppose an arithmetic expression is given as a tree.
+ *         Each leaf is an integer and each internal node is one of the standard arithmetical operations (+,−,∗,/).
+ *         For example, the expression 2 + 3 ∗ 4 + (3 ∗ 4)/5 is represented by the tree in Figure 5.17(a).
+ *         Give an O(n) algorithm for evaluating such an expression, where there are n nodes in the tree.
+ *         </p>
+ */
+public class EvaluatingExpression {
+
+    private VertexData[] verticesData;
+
+    public double eval(Graph graph, String[] verticesData) {
+        initVerticesData(verticesData);
+        DepthFirstRecursiveTraversal traversal = new DepthFirstRecursiveTraversal(graph);
+        traversal.setVertexPostProcessor(this::postProcess);
+        int start = 0;
+
+        traversal.traverse(start);
+
+        return this.verticesData[start].getNumber();
+    }
+
+    private void postProcess(GraphTraversal traversal, int vertex) {
+        VertexData vertexData = verticesData[vertex];
+
+        if (vertexData.getType() == VertexType.OPERATION) {
+            int[] adjacentVertices = traversal.getGraph().getAdjacentVertices(vertex);
+
+            if (adjacentVertices.length != 2) {
+                throw new RuntimeException("The amount of operands should equals 2");
+            }
+
+            double n1 = verticesData[adjacentVertices[0]].getNumber();
+            double n2 = verticesData[adjacentVertices[1]].getNumber();
+
+            vertexData.eval(n1, n2);
+        }
+    }
+
+    private void initVerticesData(String[] verticesData) {
+        this.verticesData = new VertexData[verticesData.length];
+
+        for (int i = 0; i < verticesData.length; i++) {
+            this.verticesData[i] = createVertexData(verticesData[i]);
+        }
+    }
+
+    private VertexData createVertexData(String data) {
+        VertexType type;
+        double number = 0;
+
+        OperationType operationType = OperationType.getOperation(data);
+        if (operationType == null) {
+            type = VertexType.NUMBER;
+            number = Double.valueOf(data);
+        } else {
+            type = VertexType.OPERATION;
+        }
+
+        return new VertexData(number, type, operationType);
+    }
+
+    private enum VertexType {
+        NUMBER, OPERATION
+    }
+
+    private enum OperationType {
+        ADD((n1, n2) -> n1 + n2),
+        SUB((n1, n2) -> n1 - n2),
+        MUL((n1, n2) -> n1 * n2),
+        DIV((n1, n2) -> n1 / n2);
+
+        private BiFunction<Double, Double, Double> f;
+
+        OperationType(BiFunction<Double, Double, Double> f) {
+            this.f = f;
+        }
+
+        static OperationType getOperation(String data) {
+            if ("+".equals(data)) {
+                return ADD;
+            } else if ("/".equals(data)) {
+                return DIV;
+            } else if ("*".equals(data)) {
+                return MUL;
+            } else if ("-".equals(data)) {
+                return SUB;
+            }
+
+            return null;
+        }
+
+        double eval(double n1, double n2) {
+            return f.apply(n1, n2);
+        }
+
+    }
+
+    private class VertexData {
+        private double number;
+        private VertexType type;
+        private OperationType operationType;
+
+        public VertexData(double number, VertexType type, OperationType operationType) {
+            this.number = number;
+            this.type = type;
+            this.operationType = operationType;
+        }
+
+        public double getNumber() {
+            return number;
+        }
+
+        public void eval(double n1, double n2) {
+            if (operationType != null) {
+                number = operationType.eval(n1, n2);
+            }
+        }
+
+        public VertexType getType() {
+            return type;
+        }
+
+    }
+
+}
---
+++ src/test/unit/ru/ildar/algorithm/graph/tasks/EvaluationExpressionTest.groovy
@@ -0,0 +1,34 @@
+package ru.ildar.algorithm.graph.tasks
+
+import ru.ildar.algorithm.graph.Graph
+import ru.ildar.algorithm.graph.GraphBuilder
+import spock.lang.Specification
+
+/**
+ * @author Ildar Gafarov (ildar.gafarov.ufa@gmail.com)
+ */
+class EvaluationExpressionTest extends Specification {
+
+    def "Test of evaluating expression that is represented as graph"() {
+        given: "Expression as graph"
+        // Using Adjacency matrix instead Adjacency List because
+        // Adjacency List gets adjacency vertices in incorrect order
+        GraphBuilder gb = GraphBuilder.adjacencyMatrix(true)
+        edges.each { edge -> gb.edge(edge[0], edge[1]) }
+        Graph graph = gb.create()
+
+        when: "Trying to evaluate expression"
+        EvaluatingExpression e = new EvaluatingExpression()
+        double result = e.eval(graph, verticesData as String[])
+
+        then: "Result should equals expected"
+        result == expectedResult
+
+        where:
+        edges                             | verticesData                                            | expectedResult
+        [[0, 1], [0, 2], [1, 3],
+         [1, 4], [2, 5], [2, 6],
+         [3, 7], [3, 8], [6, 9], [6, 10]] | ["+", "/", "+", "*", "5", "2", "*", "3", "5", "3", "4"] | 17
+    }
+
+}
